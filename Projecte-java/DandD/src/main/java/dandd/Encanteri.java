ff7176f6f920451688b409996bed0346f6d03c35
Fernando Merino <fernandomema@gmail.com>
2020-01-13 17:10:41 +0000

    afegir javadocs

--- Projecte-java/DandD/src/main/java/dandd/Encanteri.java
+++ Projecte-java/DandD/src/main/java/dandd/Encanteri.java
@@ -1,5 +1,8 @@
 package dandd;
 
+/**
+ * Encanteri utilitzable per un personatge
+ */
 class Encanteri extends Objecte {
 
     private String stat; //indica l'estat que modifica l'encanteri
@@ -7,22 +10,48 @@ class Encanteri extends Objecte {
 
     //  Getters & Setters
 
+    
+    /** 
+     * obte stat al que afecta l'encanteri
+     * @return String valor de stat
+     */
     public String getStat() {
         return stat;
     }
 
+    
+    /** 
+     * obte valor que incremeneta/decrementa a l'estat
+     * @return double valor de l'atribut quantitat
+     */
     public double getQuantitat() {
         return quantitat;
     }
 
+    
+    /** 
+     * estableix stat al que afecta l'encanteri
+     * @param quantitat valor de l'atribut quantitat
+     */
     public void setQuantitat(double quantitat) {
         this.quantitat = quantitat;
     }
 
+    
+    /** 
+     * estableix valor que incremeneta/decrementa a l'estat
+     * @param stat valor de l'atribut stat
+     */
     public void setStat(String stat) {
         this.stat = stat;
     }
 
+    
+    /** 
+     * Aplica la mijora de stat al personatge
+     * @param personatge personatge que s'aplica l'encanteri
+     * @param enemic enemic que ataca al personatge
+     */
     public void utilitzar(Personatge personatge, Enemic enemic) {
         //comprova quin a quin stat afecta la pocio
         if (stat == "atac") {
@@ -38,6 +67,11 @@ class Encanteri extends Objecte {
     }
 
     //retira els efectes aplicats al jugador
+    
+    /** 
+     * Elimina la mijora als stats aplicada per l'encanteri
+     * @param personatge
+     */
     public void estabilitzar(Personatge personatge) {
         if (stat == "atac") {
             //decrementa l'atac del personatge

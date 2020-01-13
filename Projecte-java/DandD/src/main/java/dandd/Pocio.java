e34a4d8307494d0b323c2d67dd90ac61a82f7ed7
Fernando Merino <fernandomema@gmail.com>
2020-01-13 17:04:39 +0000

    afegir javadocs

--- Projecte-java/DandD/src/main/java/dandd/Pocio.java
+++ Projecte-java/DandD/src/main/java/dandd/Pocio.java
@@ -1,21 +1,40 @@
 package dandd;
 
+/**
+ * Pocio utilitzable per un personatge
+ */
 class Pocio extends Objecte {
 
     private double vida;
 
     //  Getters & Setters
 
+    
+    /** 
+     * obte el valor de vida
+     * @return double valor de vida
+     */
     public double getVida() {
         return vida;
     }
 
+    
+    /** 
+     * Estableix el valor de vida
+     * @param vida valor de vida
+     */
     public void setVida(double vida) {
         this.vida = vida;
     }
 
     // Metodes
 
+    
+    /** 
+     * Aplica la vida de la pocio a un personatge
+     * @param personatge personatge que utilitza la pocio
+     * @param enemic enemic que ataca al personatge
+     */
     public void utilitzar(Personatge personatge, Enemic enemic) { //s'utilitza
         personatge.setVida(personatge.getVida() + this.getVida()); // s'augmenta la vida del personatge
         enemic.atacar(personatge);
